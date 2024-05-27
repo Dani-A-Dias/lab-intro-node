a516cd30b151113cc2cb4dde5e888417d45fd2cd
DaniDias95 <guiguimu_art@hotmail.com>
2024-05-27 17:28:53 +0100

    Lab Done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=> a-b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos<0 || pos>= this.items.length){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length === 0){
+      return 0
+    }
+    let sumAll = this.items.reduce((arr,curr)=>arr+curr, 0)
+    return sumAll
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    let sumAll = this.sum()
+    return sumAll/this.items.length
+    
+  }
 }
 
 module.exports = SortedList;
